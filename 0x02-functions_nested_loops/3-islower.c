020177ed29da47ee9bd897be46a51d33137c7873
Aystar <gideonayomide75@gmail.com>
2022-12-13 13:48:43 -0800

    C project part 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,7 +7,7 @@
  * Return: 1 if lowercase, 0 otherwise
  */
 
-int _islower(int c)
+in _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  * Return: 1 if alphabet, 0 otherwise
  */
 
-int _isalpha(int c)
+in _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
