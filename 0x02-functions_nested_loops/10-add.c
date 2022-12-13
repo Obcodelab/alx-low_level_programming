708df2e52d6349278afc4f2fafabcdabe9339a12
Aystar <gideonayomide75@gmail.com>
2022-12-13 14:42:39 -0800

    C project part 3

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	sum = a + b;
+	return (sum);
+}
