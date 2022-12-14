b4ecefbfe7e4b7e0bced8334ce29fec217fd3bd8
Aystar <gideonayomide75@gmail.com>
2022-12-13 16:05:53 -0800

    C project part 3

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - list multiples of 3 or 5 below 1024
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
