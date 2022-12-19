0dfa4942d1ceeb2fbb0a50dd644354387f9e8e48
Aystar <gideonayomide75@gmail.com>
2022-12-19 07:27:50 -0800

    C project part 6

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,13 +10,11 @@ void puts_half(char *str)
 	int string = 0, n;
 
 	while (str[string] != '\0')
-	{
 		string++;
-		if (string % 2 == '0')
-			n = (string / 2);
-		else
-			n = (string - 1) / 2;
-	}
+	if (string % 2 == '0')
+		n = (string / 2);
+	else
+		n = (string - 1) / 2;
 	n++;
 
 	for (string = n; str[string] != '\0'; string++)
