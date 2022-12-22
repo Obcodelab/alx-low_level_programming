854dfd14a341a905f4bd5cf9d4bb363c642ef6f2
Aystar <gideonayomide75@gmail.com>
2022-12-22 12:00:04 -0800

    C project part 7

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -33,7 +33,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			break;
 		}
 		m = n / 10;
-		r[k] = (n % 10) + '0';
+		r[k] = n % 10 + '0';
 	}
 	r[k] = '\0';
 	if (i >= 0 || j >= 0 || m)
