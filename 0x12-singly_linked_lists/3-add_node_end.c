116548aa28fa3312a9fe90547437524912665d6b
Aystar <gideonayomide75@gmail.com>
2023-01-26 05:01:59 -0800

    C project part 19

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -22,22 +22,22 @@ int _strlen(const char *str)
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *tmp; /* create new node */
+	list_t *new_node, *tmp;
 
-	if (str == NULL) /* validate input */
+	if (str == NULL)
 		return (NULL);
-	if (strdup(str) == NULL) /* check if malloc errored */
+	if (strdup(str) == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(list_t)); /* malloc for new node */
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str); /* set node values */
+	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
 
-	if (*head == NULL) /* if no list nodes, set new_node to beginning */
+	if (*head == NULL)
 		*head = new_node;
 	else
 	{
