8ca0988987a792236d636d42e1ba508d560faffe
Aystar <gideonayomide75@gmail.com>
2023-01-26 04:59:56 -0800

    C project part 19

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,55 +1,52 @@
 #include "lists.h"
 
 /**
- * _strlen - finds the length of a string
- * @str: the string
- *
- * Return: length of string
+ * _strlen - find string length
+ * @str: string
+ * Return: length
  */
-
 int _strlen(const char *str)
 {
-	int i;
+	int len;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (len = 0; str[len] != '\0'; len++)
 		;
-	return (i);
+	return (len);
 }
 
 /**
- * add_node_end - adds a new node at the end of a list_t list.
- * @head: double pointer to a linked list
- * @str: the string
- *
- * Return: the address of new element
+ * add_node_end - add node to end of linked list
+ * @head: linked list
+ * @str: data for new node
+ * Return: address of new element, or NULL if failed
  */
-
-list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *tmp;
+	list_t *new_node, *tmp; /* create new node */
 
-	if (str == NULL)
+	if (str == NULL) /* validate input */
 		return (NULL);
-	if (strdup(str) == NULL)
-		return (NULL);
-
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	if (strdup(str) == NULL) /* check if malloc errored */
 		return (NULL);
 
-	new->str = strdup(str);
-	new->i = _strlen(str);
-	new->next = NULL;
+	new_node = malloc(sizeof(list_t)); /* malloc for new node */
+	if (new_node == NULL)
+		return (NULL);
 
-	if (*head == NULL)
-		*head = new;
+	new_node->str = strdup(str); /* set node values */
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL) /* if no list nodes, set new_node to beginning */
+		*head = new_node;
 	else
 	{
 		tmp = *head;
 
 		while (tmp->next != NULL)
 			tmp = tmp->next;
-		tmp->next = new;
+		tmp->next = new_node;
 	}
-	return (new);
+
+	return (new_node);
 }
