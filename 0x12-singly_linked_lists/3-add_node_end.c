dbc1a49e222717a459eb59b706c836d687b10bd8
Aystar <gideonayomide75@gmail.com>
2023-01-26 04:57:17 -0800

    C project part 19

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,11 +7,11 @@
  * Return: length of string
  */
 
-unsigned int _strlen(char *str)
+int _strlen(const char *str)
 {
-	unsigned int i;
+	int i;
 
-	for (i = 0; str[i]; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
 	return (i);
 }
@@ -30,25 +30,26 @@ list_t *add_node_end(list_t **head, const char *str);
 
 	if (str == NULL)
 		return (NULL);
+	if (strdup(str) == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = _strlen(new->str);
+	new->i = _strlen(str);
 	new->next = NULL;
+
 	if (*head == NULL)
-	{
 		*head = new;
-		return (new);
+	else
+	{
+		tmp = *head;
+
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
 	}
-	tmp = *head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
 	return (new);
 }
