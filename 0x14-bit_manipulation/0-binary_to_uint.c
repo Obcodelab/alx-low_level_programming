264c84f4df55dcfd51f40095bae9f242acd40ed7
Aystar <gideonayomide75@gmail.com>
2023-02-02 08:34:28 -0800

    C project part 21

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -26,5 +26,5 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] == '1')
 			j += 1;
 	}
-	return (j)
+	return (j);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	i = 0 << index;
+	i = ~(1 << index);
 	*n = *n & i;
 
 	return (i);
